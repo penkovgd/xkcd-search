a4927298f2ba0dab61e2ee0d0b7ba73ce88093c6
George Penkov <penkov.gd@gmail.com>
2025-12-20 15:14:01 +0300

    init web microservice

---
+++ search-services/web/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { NButton } from 'naive-ui'
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+    <n-button>naive-ui</n-button>
+</template>
+
+<style scoped></style>
